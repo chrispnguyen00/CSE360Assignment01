ebcfeee4d57b0f797ba557a0260204ea140b20d4
Chris <Chris@LAPTOP-QIBM4GOD>
2020-10-05 15:31:13 -0700

    finished the assignment and tested it by creating a main function

--- Assignment01/src/cse360assignment01/AddingMachine.java
+++ Assignment01/src/cse360assignment01/AddingMachine.java
@@ -2,6 +2,12 @@ package cse360assignment01;
 
 public class AddingMachine {
 	  private int total;
+	  private StringBuffer str;
+	  
+	  public AddingMachine() {
+		  total = 0;
+		  str = new StringBuffer("0");
+	  }
 	  
 	  public int getTotal () { 
 	    return total;
@@ -9,24 +15,32 @@ public class AddingMachine {
 	  
 	  public void add (int value) {
 		  total += value;
+		  str.append(" + " + value);
+		  
 	  }
 
 	  public void subtract (int value) {
 		  total -= value;
+		  str.append(" - " + value);
 	  }
 
 	  public String toString () {
-		  String str = "";
-		  //
+		  System.out.println(str);
 		  return "";
 	  }
 
 	  public void clear() {
+		  total = 0;
+		  str = new StringBuffer("0");
 		  
 	  }
 	  
 	  public static void main(String[] args) {
-		  
+		  AddingMachine myCalculator = new AddingMachine();
+		  myCalculator.add(4);
+		  myCalculator.subtract(2);
+		  myCalculator.add(5);
+		  myCalculator.toString();
 	  }
 }
 
